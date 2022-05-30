3377a8127c504ab560dc910e3c1c81b2797de3e1
Carrington <mcarringtone@gmail.com>
2022-05-30 11:32:37 -0400

    app.use

--- server.js
+++ server.js
@@ -3,3 +3,7 @@ const mongoose = require("mongoose");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(require("./routes"));

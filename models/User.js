5c748d267589dce882b3003c012fbb0ed4ae277b
Carrington <mcarringtone@gmail.com>
2022-05-30 11:29:24 -0400

    user controller

--- models/User.js
+++ models/User.js
@@ -1,32 +1,34 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        match: [/.+@.+\..+/]
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/],
     },
-    thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
-    friends: [{ type: Schema.Types.ObjectId, ref: 'User' }]
-},
-{
+    thoughts: [{ type: Schema.Types.ObjectId, ref: "Thought" }],
+    friends: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  },
+  {
     toJSON: {
-        virtuals: true
+      virtuals: true,
     },
-    id: false
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
 
-userSchema.virtual('friendCount').get(function() {
-    return this.friends.length;
-});
-
-const User = model('User', userSchema);
+const User = model("User", userSchema);
 
 module.exports = User;
